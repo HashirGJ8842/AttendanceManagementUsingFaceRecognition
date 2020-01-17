6705bde3dc589ad969090af49be8b6c235315a62
HashirGJ8842 <56411583+HashirGJ8842@users.noreply.github.com>
2020-01-17 15:05:35 +0530

    Minor Changes to resolve errors

--- Graphical_Interface.py
+++ Graphical_Interface.py
@@ -8,12 +8,14 @@ import time
 import Employee
 
 
-class PhotoBoothApp:
+class MarkingAttendance:
+    def func(self):
+        self.thread = threading.Thread(target=self.MA_videoLoop, args=())
+        self.thread.start()
 
-        
     def MA_videoLoop(self):
-        recognizer = cv2.face.LBPHFaceRecognizer_create()
-        recognizer.read('trainer/trainer.yml')
+        #recognizer = cv2.face.LBPHFaceRecognizer_create()
+        #recognizer.read('trainer/trainer.yml')
         cascadePath = "haarcascade_frontalface_default.xml"
         faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + cascadePath)
         font = cv2.FONT_HERSHEY_SIMPLEX
@@ -21,11 +23,11 @@ class PhotoBoothApp:
         minW = 0.1 * self.vs.get(3)
         minH = 0.1 * self.vs.get(4)
         confidence = 0
-        
-            
-            
+
+
+
         while True:
-            
+
             ret ,self.frame = self.vs.read()
             gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
             image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
@@ -41,7 +43,7 @@ class PhotoBoothApp:
 
             for (x, y, w, h) in faces:
                 cv2.rectangle(self.frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+         #       id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
                 #If confidence is less them 100 ==> "0" : perfect match
                 if (confidence < 100):
@@ -49,28 +51,28 @@ class PhotoBoothApp:
                     confidence = round(100 - confidence)
                 else:
                     id = "unknown"
-                    confidence = "  {0}%".format(round(100 - confidence))
+                    confidence_percent = "  {0}%".format(round(100 - confidence))
                     confidence = round(100 - confidence)
-                
 
-                cv2.putText(
-                   self.frame,
-                   str(id),
-                   (x + 5, y - 5),
-                   font,
-                    1,
-                    (255, 255, 255),
-                    2
-                )                    
-                cv2.putText(
-                    self.frame,
-                    str(confidence),
-                    str((x + 5, y + h - 5)),
-                    font,
-                    1,                    
-                    str((255, 255, 0)),
-                    1
-                 )
+
+                #cv2.putText(
+                #   self.frame,
+                #   str(id),
+                #   (x + 5, y - 5),
+                #   font,
+                #    1,
+                #    (255, 255, 255),
+                #    2
+                #)
+                #cv2.putText(
+                #    self.frame,
+                #    str(confidence),
+                #    str((x + 5, y + h - 5)),
+                #    font,
+                #    1,
+                #    str((255, 255, 0)),
+                #    1
+                # )
 
             if confidence > 70:
                 #print("Match Found, Marking Attendance!")
@@ -78,19 +80,19 @@ class PhotoBoothApp:
                 #print("Marking Attendance")
                 #print(result)
                 self.label1.config(text="Match Found, Marking Attendance!", bg="lightgreen")
-                
-            
+
+
             if self.panel is None:
                 self.panel = tki.Label(image=image)
                 self.panel.image = image
                 self.panel.pack(side="left", padx=10, pady=10)
 
-            
+
             else:
                 self.panel.configure(image=image)
                 self.panel.image = image
-            cv2.waitKey(1)            
-        
+            cv2.waitKey(1)
+
 
     def onClose(self):
         print("[INFO] closing...")
@@ -108,29 +110,24 @@ class PhotoBoothApp:
         self.root = tki.Tk()
         self.panel = None
 
-        
-        self.stopEvent = threading.Event()
-        self.thread = threading.Thread(target=self.MA_videoLoop, args=())
-        self.thread.start()
-
 
         self.vs.set(3, 480)
         self.vs.set(4, 480)
-        
-        
+        self.stopEvent = threading.Event()
+
         self.label1 = tki.Label(self.root)
         self.label1.pack(side="bottom", fill="both", expand="yes", padx=10,
                          pady=10)
         btn1 = tki.Button(self.root, text="Train")
         btn1.pack(side="bottom", fill="both", expand="yes", padx=10,
                  pady=10)
-        btn2 = tki.Button(self.root, text="Mark Attendance", command=self.MA_videoLoop)
+        btn2 = tki.Button(self.root, text="Mark Attendance", command=self.func)
         btn2.pack(side="bottom", fill="both", expand="yes", padx=10,
                   pady=10)
         btn3 = tki.Button(self.root, text="Display")
         btn3.pack(side="bottom", fill="both", expand="yes", padx=10,
                   pady=10)
-        
+
         self.root.wm_title("PyImageSearch PhotoBooth")
         self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
 
@@ -140,5 +137,5 @@ vs = cv2.VideoCapture(0)
 time.sleep(2.0)
 
 
-pba = PhotoBoothApp(vs)
+pba = MarkingAttendance(vs)
 pba.root.mainloop()
