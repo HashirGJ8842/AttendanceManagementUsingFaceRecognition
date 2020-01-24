96d5e11f14b30d52af98f7ab70679c95f4064670
HashirGJ8842 <56411583+HashirGJ8842@users.noreply.github.com>
2020-01-24 18:35:34 +0530

    Added functionality to the Train button

--- Graphical_Interface.py
+++ Graphical_Interface.py
@@ -6,12 +6,86 @@ import threading
 import cv2
 import time
 import Employee
+import os
 
 
 class MarkingAttendance:
-    def func(self):
+    def function_MA(self):
         self.thread = threading.Thread(target=self.MA_videoLoop, args=())
         self.thread.start()
+
+    def function_TI(self):
+        self.thread = threading.Thread(target=self.TI_videoLoop, args=())
+        self.thread.start()
+
+    def TI_videoLoop(self):
+        os.chdir("train_data")
+
+        def checkDirectory(emp_id):
+            """
+            Check if a directory for that employee id exists. If it exits then change directory and save the new images to it.
+            Else create a directory,change to it and store in it.
+            :return:
+            """
+            if str(emp_id) in os.listdir(os.getcwd()):
+                os.chdir(str(emp_id))
+                return "Changed Directory to " + str(emp_id)
+            else:
+                os.mkdir(str(emp_id))
+                os.chdir(str(emp_id))
+                return "Creating a new directory"
+
+        def captureImage(emp_id):
+            i = 0
+            checkDirectory(emp_id)
+            while i < 30:
+                #raw_input('Press Enter to capture')
+                return_value, self.frame = self.vs.read()
+                gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+                image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+                image = Image.fromarray(image)
+                image = ImageTk.PhotoImage(image)
+                faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+                faces = faceCascade.detectMultiScale(
+                    gray,
+                    scaleFactor=1.3,
+                    minNeighbors=3,
+                    minSize=(30, 30)
+                )
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(self.frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                height, width = self.frame.shape[:2]
+
+                for (x, y, w, h) in faces:
+                    r = max(w, h) / 2
+                    centerx = x + w / 2
+                    centery = y + h / 2
+                    nx = int(centerx - r)
+                    ny = int(centery - r)
+                    nr = int(r * 2)
+
+                    faceimg = self.frame[ny:ny + nr, nx:nx + nr]
+                    lastimg = cv2.resize(faceimg, (512, 512))
+                    status = cv2.imwrite(str(emp_id) + '_' + str(i) + '.jpg', lastimg)
+                if self.panel is None:
+                    self.panel = tki.Label(image=image)
+                    self.panel.image = image
+                    self.panel.pack(side="left", padx=10, pady=10)
+
+
+                else:
+                    self.panel.configure(image=image)
+                    self.panel.image = image
+
+
+                print("[INFO] Found {0} Faces!".format(len(faces)))
+                i += 1
+                self.label1.config(text=f'Took {i} images, found {len(faces)} faces', bg='lightgreen')
+
+
+        captureImage(self.entry.get())
+
 
     def MA_videoLoop(self):
         recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -83,6 +157,11 @@ class MarkingAttendance:
                 self.panel.image = image
             cv2.waitKey(1)
 
+    def enter_bar(self):
+        self.entry.pack(side="bottom", fill="both", expand="yes", padx=10,
+                        pady=10)
+        self.btn1.config(text="Proceed", command=self.function_TI)
+
     def onClose(self):
         print("[INFO] closing...")
         self.stopEvent.set()
@@ -105,17 +184,18 @@ class MarkingAttendance:
         self.label1 = tki.Label(self.root)
         self.label1.pack(side="bottom", fill="both", expand="yes", padx=10,
                          pady=10)
-        btn1 = tki.Button(self.root, text="Train")
-        btn1.pack(side="bottom", fill="both", expand="yes", padx=10,
+        self.btn1 = tki.Button(self.root, text="Train", command=self.enter_bar)
+        self.btn1.pack(side="bottom", fill="both", expand="yes", padx=10,
+                       pady=10)
+        self.entry = tki.Entry(self.root)
+        self.btn2 = tki.Button(self.root, text="Mark Attendance", command=self.function_MA)
+        self.btn2.pack(side="bottom", fill="both", expand="yes", padx=10,
                   pady=10)
-        btn2 = tki.Button(self.root, text="Mark Attendance", command=self.func)
-        btn2.pack(side="bottom", fill="both", expand="yes", padx=10,
-                  pady=10)
-        btn3 = tki.Button(self.root, text="Display")
-        btn3.pack(side="bottom", fill="both", expand="yes", padx=10,
+        self.btn3 = tki.Button(self.root, text="Display")
+        self.btn3.pack(side="bottom", fill="both", expand="yes", padx=10,
                   pady=10)
 
-        self.root.wm_title("PyImageSearch PhotoBooth")
+        self.root.wm_title("Face Recognition")
         self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
 
 
